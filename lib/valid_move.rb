0a3eba08374c7ae8d1dd8d7f35bf49999d9c5ba8
Jeremy Amram <jeremy.j.amram@gmail.com>
2019-07-28 00:32:11 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -25,6 +25,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken?(board,index) 
-  end
+  if index.between?(0,8) && !position_taken?(board,index) true
+else false
+end
 end
